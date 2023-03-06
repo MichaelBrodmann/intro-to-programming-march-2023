373a113984b9d4c5e3049d2af15f9cb9ad50c7bb
Michael Brodmann <Michael_e_Brodmann@progressive.com>
2023-03-06 12:32:15 -0500

    before lunch Monday

---
+++ StatusSolution/Status/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Status;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+app.MapGet("/status", () =>
+{
+    var statusMessage = new StatusMessage(Guid.NewGuid(), "Looks good", DateTimeOffset.Now);
+    return Results.Ok(statusMessage);
+});
+
+app.Run();
+
+
