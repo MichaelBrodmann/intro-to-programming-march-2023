3bead8cbb698c324e4ea655439f85d6e37c3948e
Michael Brodmann <Michael_e_Brodmann@progressive.com>
2023-03-08 12:28:00 -0500

    inheritance example

--- BankingSolution/Banking.UnitTests/GoldCustomerDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerDeposits.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Banking.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Banking.UnitTests
+namespace Banking.UnitTests;
+
+public class GoldCustomerDeposits
 {
-    internal class GoldCustomerDeposits
+    [Fact]
+    public void GoldCustomersGetABonusOnDeposits()
     {
+        var account = new GoldBankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M; 
+
+        account.Deposit(amountToDeposit); 
+
+        Assert.Equal(amountToDeposit + openingBalance + 10M, account.GetBalance());
     }
 }
