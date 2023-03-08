922c1d5361638c28e29b5a8bd06df3e8da2b6b15
Michael Brodmann <Michael_e_Brodmann@progressive.com>
2023-03-08 16:00:56 -0500

    End of Day 3

---
+++ stringcalculator-wed-8/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers.Contains(",")) {
+            var commaAt = numbers.IndexOf(',');
+            string x = numbers.Substring(0, commaAt);
+            string y = numbers.Substring(commaAt+1);
+            return int.Parse(x) + int.Parse(y);
+        }
+        
+       
+        if (numbers.Any(char.IsDigit)) {
+            return int.Parse(numbers);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ stringcalculator-wed-8/StringCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Theory]
+    [InlineData(10,"10")]
+    [InlineData(15, "15")]
+    [InlineData(0, "")]
+    [InlineData(10,"5,5")]
+    [InlineData(8, "5,  3")]
+    [InlineData(25, "12,13")]
+    public void EmptyStringReturnsZero(int expected, string actual)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(actual);
+
+        Assert.Equal(expected, result);
+    }
+}
