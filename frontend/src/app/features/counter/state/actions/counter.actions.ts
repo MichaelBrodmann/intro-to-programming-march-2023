5d4d57dfdccf12c483c81729f1d3a0ad03daf8ce
Michael Brodmann <Michael_e_Brodmann@progressive.com>
2023-03-14 15:48:04 -0400

    Ship the counter component

---
+++ frontend/src/app/features/counter/state/actions/counter.actions.ts
@@ -0,0 +1,10 @@
+import { createActionGroup, emptyProps } from '@ngrx/store';
+
+export const counterEvents = createActionGroup({
+  source: 'Counter',
+  events: {
+    'Increment Button Clicked': emptyProps(),
+    'Decrement Button Clicked': emptyProps(),
+    'Reset Button Clicked': emptyProps(),
+  },
+});
